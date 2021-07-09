40cb11a8e450ccce2e85bb32fdb2aca90b8c1b59
Lokesh Rangineni <lokeshforjava@gmail.com>
2021-07-09 03:04:44 -0400

    Adding the Load testing scenarios for the kogito travel request example application.
    Updated README.md file about Travel Request scenario.
    
    * Updated readme file with detailed instructions.
    * Added load testing of process spring boot examples as well.
    * Cleaned up some code.
    
    Updated json payload.

--- src/test/scala/com/redhat/kogito/gatling/scenarios/PostOrder.scala
+++ src/test/scala/com/redhat/kogito/gatling/scenarios/PostOrder.scala
@@ -6,22 +6,33 @@
  * This simulation try to emulate below API call.
  * curl -d '{"approver" : "john", "order" : {"orderNumber" : "12345", "shipped" : false}}' -H "Content-Type: application/json" -X POST http://localhost:8080/orders
  *
- *http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/orders-resource/createResource_orders
+ * http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config#/orders-resource/createResource_orders
  */
 package com.redhat.kogito.gatling.scenarios
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
 
 object PostOrder {
 
-  val orderString = "{\"approver\" : \"john\", \"order\" : {\"orderNumber\" : \"12345\", \"shipped\" : false}}"
+  val orderJsonString: String =
+    """
+      |{
+      |  "approver": "john",
+      |  "order": {
+      |    "orderNumber": "12345",
+      |    "shipped": false
+      |  }
+      |}
+      |""".stripMargin
 
-  val postOrderHttp = http("HTTP Post Order")
+  val postOrderHttp: HttpRequestBuilder = http("HTTP Post Order")
     .post("/orders")
-    .body(StringBody(orderString)).asJson
+    .body(StringBody(orderJsonString)).asJson
     .check(status is 201)
 
-  val postOrder = scenario(postOrderHttp.toString)
+  val postOrder: ScenarioBuilder = scenario(postOrderHttp.toString)
     .exec(postOrderHttp)
 }
--- src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
+++ src/test/scala/com/redhat/kogito/gatling/util/Environment.scala
@@ -5,6 +5,6 @@ package com.redhat.kogito.gatling.util
 object Environemnt {
   val baseURL = scala.util.Properties.envOrElse("baseURL", "http://localhost:8080")
   val users = scala.util.Properties.envOrElse("numberOfUsers", "5000")
-  val maxResponseTime = scala.util.Properties.envOrElse("maxResponseTime", "5000") // in milliseconds
+  val maxResponseTime = scala.util.Properties.envOrElse("maxResponseTime", "500000") // in milliseconds
 
 }
